931b0c6883190ccf1241de708add4042abe6b5c3
mohamedsu <Mohamed2.Suoodh@live.uwe.ac.uk>
2020-09-14 05:54:06 +0500

    product list and product

--- src/context/products.js
+++ src/context/products.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import url from '../utils/URL';
 
 export const ProductContext = React.createContext();
 
 // Provider, Consumer, useContext()
 
 const ProductProvider = ({ children }) => {
-  const greeting = 'hello';
-  const product = { id: 1, titile: 'product name' };
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [featured, setFeatured] = useState([false]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`${url}/products`).then((response) => {
+      setProducts(response.data);
+      setLoading(false);
+    });
+    return () => {};
+  }, []);
+
   return (
-    <ProductContext.Provider value={{greeting, product}}>{children}</ProductContext.Provider>
+    <ProductContext.Provider value={{ loading, products, featured }}>
+      {children}
+    </ProductContext.Provider>
   );
 };
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,14 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { ProductContext } from '../context/products';
+import Loading from '../components/Loading';
+import ProductList from '../components/Products/ProductList';
 
 export default function Products() {
-  const {greeting} = React.useContext(ProductContext);
+  const { loading, products } = useContext(ProductContext);
 
-  return <h1>hello from products page {greeting} </h1>;
+  if (loading) {
+    return <Loading />;
+  }
+
+  return <ProductList title="our products" products={products} />;
 }
