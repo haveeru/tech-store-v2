d0d6b3bd6563c60dfcd2b0edcac41df3bf6b7842
mohamedsu <Mohamed2.Suoodh@live.uwe.ac.uk>
2020-09-14 23:32:32 +0500

    cat context setup

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,1 +1,19 @@
 // cart context
+import React, { useState } from 'react';
+import localCart from '../utils/localCart';
+
+const CartContext = React.createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(localCart);
+  const [total, setTotal] = useState(0);
+  const [cartItem, setCartItem] = useState(0);
+
+  return (
+    <CartContext.Provider value={{ cart, total, cartItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartContext, CartProvider };
