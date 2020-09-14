15b28c32ed549fc7b2d0ae8db5cf7bc3165f99e3
mohamedsu <Mohamed2.Suoodh@live.uwe.ac.uk>
2020-09-14 23:49:31 +0500

    Empty cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/cart';
+import EmptyCart from '../components/Cart/EmptyCart';
+import CartItem from '../components/Cart/CartItem';
+import { Link } from 'react-router-dom';
+//import { userContext } from '../context/user';
 
 const Cart = () => {
+  let user = false;
   const { cart, total } = useContext(CartContext);
 
-  console.log({ cart, total });
+  if(cart.length === 0) return <EmptyCart />
+
   return <h1>hello from cart page</h1>;
 };
 
