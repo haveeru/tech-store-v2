19407d09792bd75c1d9ae641a077c6604f5480f5
haveeru <suood_mohamed@hotmail.com>
2020-09-30 10:21:19 +0500

    submit order setup 67

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -32,7 +32,27 @@ function Checkout(props) {
 
     const { token } = response;
     if (token) {
-      console.log(response);
+      // submit order
+      setError('');
+      const { id } = token;
+      let order = await submitOrder({
+        name: name,
+        total: total,
+        items: cart,
+        stripeTokenId: id,
+        userToken: user.token,
+      });
+      if (order) {
+        showAlert({ msg: 'your order is complete' });
+        clearCart();
+        history.push('./');
+        return;
+      } else {
+        showAlert({
+          msg: 'there was an error with your order. please try again!',
+          type: 'danger',
+        });
+      }
     } else {
       hideAlert();
       setError(response.error.message);
